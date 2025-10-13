7ac9dbcbf2568eb48452dc6c955e7900dc5be1d8
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2025-10-13 10:27:43 +0200

    Removed obsolete text about AT commands
    There was an obsolete text that mentioned using AT commands to
    verify the Thingy:53 firmware.
    
    Signed-off-by: Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>

--- src/common/steps/Thingy53Verify/index.tsx
+++ src/common/steps/Thingy53Verify/index.tsx
@@ -116,7 +116,7 @@ const VerifyStep = ({
         <Main>
             <Main.Content
                 heading={getHeading()}
-                subHeading="Automatically verify kit communication with AT commands."
+                subHeading="Automatically verify kit communication with Matter shell commands."
             >
                 <div className="tw-flex tw-flex-col tw-items-start tw-justify-start tw-gap-4">
                     {commands.map(({ title, copiable = false }, index) => (
